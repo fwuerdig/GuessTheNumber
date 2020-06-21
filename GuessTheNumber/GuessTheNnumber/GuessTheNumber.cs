ccafac28bdc3935f5221f8fa7aa57a2009560f24
Fabio Wuerdig <fabio.wuerdig@gmail.com>
2020-06-21 19:55:54 +0100

    Add files via upload

---
+++ GuessTheNumber/GuessTheNnumber/GuessTheNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GuessTheNumber
+{
+    public class GuessTheNumber
+    {
+        int number;
+        int max;
+
+        public GuessTheNumber()
+        {
+            Random random = new Random();
+            max = 100;
+            number = Math.Abs(random.Next()) % (max + 1);
+        }
+
+        public void play()
+        {
+            while (true)
+            {
+                int move = Convert.ToInt32(Console.ReadLine());
+                if (move > number)
+                    Console.WriteLine("Your number is too big!");
+                else if (move < number)
+                    Console.WriteLine("Your number is too small!");
+                else
+                    Console.WriteLine("YOU GOT IT BRO!!!");                  
+                    
+            }               
+
+        }
+        static void Main(string[] args)
+        {
+            GuessTheNumber guessGame = new GuessTheNumber();
+            Console.WriteLine("Welcome to my game. Try and guess the number. It´s between 0 and " 
+                + guessGame.max + " inclusive. Just put a number to begin");
+            guessGame.play();
+        }
+    }
+}
